37b78ae32abeb05f6f2d716a7e7b5f6c12447fc5
Rayane Cavalcanti <rayaneescola32@gmail.com>
2025-05-25 19:48:19 -0300

    feature: HUs Épico home

---
+++ src/pages/home/components/Botoes.jsx
@@ -0,0 +1,30 @@
+import styles from './Botoes.module.scss';
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const Botoes = () => {
+    function click() {
+        toast.warning('Funcionalidade em construção! Em breve disponível em sistema.', {
+            position: "top-right"
+        });
+    }
+
+    return (
+        <div className={styles.BotoesContainer}>
+            <div onClick={click} className={`${styles.icon} material-icons`}>
+                <span>location_on</span>
+            </div>
+            <div onClick={click} className={styles.localizacao}>
+                Salvar localização em tempo real
+            </div>
+            <div onClick={click} className={`${styles.icon} ${styles.sos}`}>
+                <span>SOS</span>
+            </div>
+
+            <ToastContainer />
+        </div>
+    );
+};
+
+export default Botoes;
